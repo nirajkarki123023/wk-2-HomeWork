f466095192fde122e575b5b7a9cf998ea65095ae
Niraj Karki <nirajjungkarki@mail.weber.edu>
2026-02-03 23:39:17 -0700

    Implement item pricing and receipt generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,38 @@
-//TODO: add the appropriate include statements (iostream and string)
+#include <iostream>
+#include <string>
+using namespace std;
 
-//TODO: create the main function
+int main()
 {
-    //TODO: make three variables: itemName (type string), price (type double), and quantity (type int)
-
+    string itemName;
+    double price;
+    int quantity;
+    double subtotal;
+    double tax;
+    double total;
     const double TAX_RATE = 0.07;
-
-    std::cout << "Enter item name: ";
-    std::cin >> itemName;
-
-    //TODO: print the following to the screen: "Enter price per item: "
-    //TODO: have the user enter the price
-
-    //TODO: print the following to the screen: "Enter quantity: "
-    //TODO: have the user enter the quantity
-
-    //TODO: Set values to the subtotal, tax, and total. The subtotal would be the total price before taxes. The tax would be the subtotal multiplied by the tax rate, and 
-    //      the total would be the total plus the tax
-
-    // Output
-    std::cout << "\n----- RECEIPT -----\n";
-    std::cout << "Item: " << itemName << std::endl;
-    std::cout << "Price: $" << price << std::endl;
-    std::cout << "Quantity: " << quantity << std::endl;
-    std::cout << "Subtotal: $" << subtotal << std::endl;
-    std::cout << "Tax (7%): $" << tax << std::endl;
-    std::cout << "Total: $" << total << std::endl;
-    std::cout << "-------------------\n";
-
-//TODO: remember the return 0!
+    
+    cout << "Enter item name: ";
+    cin >> itemName;
+    
+    cout << "Enter price per item: ";
+    cin >> price;
+    
+    cout << "Enter quantity: ";
+    cin >> quantity;
+    
+    subtotal = price * quantity;
+    tax = subtotal * TAX_RATE;
+    total = subtotal + tax;
+    
+    cout << "\n----- RECEIPT -----\n";
+    cout << "Item: " << itemName << endl;
+    cout << "Price: $" << price << endl;
+    cout << "Quantity: " << quantity << endl;
+    cout << "Subtotal: $" << subtotal << endl;
+    cout << "Tax (7%): $" << tax << endl;
+    cout << "Total: $" << total << endl;
+    cout << "-------------------\n";
+    
+    return 0;
 }
